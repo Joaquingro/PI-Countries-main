977b6d019952b06ec1a356780b000152dce76d01
Joaquingro <joaquinsgro@gmail.com>
2023-04-24 21:36:11 -0600

    restructuring code

--- PI-Countries-main/api/src/db.js
+++ PI-Countries-main/api/src/db.js
@@ -3,12 +3,12 @@ const { Sequelize, Op } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 const modelCountry = require("../src/models/Country");
 const modelActivity = require("../src/models/Activity");
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
